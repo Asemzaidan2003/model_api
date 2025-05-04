807390cc202602220435be12a2490b397e6f1212
Asem Zaidan <asemfas@gmail.com>
2025-05-04 18:04:33 +0300

    fixing git add .

--- app/main.py
+++ app/main.py
@@ -2,4 +2,8 @@ from fastapi import FastAPI
 from app.routes.predict import router as predict_router
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Render!"}
 app.include_router(predict_router, prefix="/predict")
