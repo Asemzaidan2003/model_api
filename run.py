99725c0856298ae34e196748c0192ddfdd10001b
Asem Zaidan <asemfas@gmail.com>
2025-05-04 18:16:33 +0300

    nothing important

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 from app.main import app
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=os.getenv("PORT"))
+    uvicorn.run(app, host="0.0.0.0", port=10000)
